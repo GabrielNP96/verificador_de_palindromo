446f72554e6f59d9d0b03d39d9f4ec0acc55364d
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-02-25 19:20:42 +0000

    verificador pronto

--- src/checkPalindromo.ts
+++ src/checkPalindromo.ts
@@ -2,7 +2,13 @@ export function checkPalindromo(str : string) : string {
     const strFormatted: string = str.replace(/[\s\p{P}]+/gu, "");
     const wordStr: string = strFormatted.split("").reverse().join("");
 
+    if(strFormatted.length < 2) {
+        return 'Você precisa escrever ao menos um caractere';
+    }
+    
     if (strFormatted === wordStr) {
-        
+        return `${str} é um palíndromo`;
+    } else {
+        return `${str} não é um palindromo`
     }
 }
