3c43c67f70fa538afe0cf0faa486000b076a7b9e
GabrielNP96 <123754383+GabrielNP96@users.noreply.github.com>
2025-02-26 15:32:11 +0000

    Deploying to gh-pages from @ GabrielNP96/verificador_de_palindromo@109faf28ae96f3c47c67dc94155cdf10c701e0f8 🚀

---
+++ js/checkPalindromo.js
@@ -0,0 +1,13 @@
+export function checkPalindromo(str) {
+    const strFormatted = str.replace(/[\s\p{P}]+/gu, "").toLowerCase();
+    const wordStr = strFormatted.split("").reverse().join("").toLowerCase();
+    if (strFormatted.length < 2) {
+        return '<p class = "not-palindrome" >Você precisa escrever ao menos um caractere</p>';
+    }
+    if (strFormatted === wordStr) {
+        return `<p class= "palindrome">${str} é um palíndromo</p>`;
+    }
+    else {
+        return `<p class = "not-palindrome">${str} não é um palíndromo</p>`;
+    }
+}
